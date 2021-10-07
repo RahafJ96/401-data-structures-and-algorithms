f9974491f63dab9dd7e7801263f53c23c773434b
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-07 21:33:11 +0300

    stack and queue

---
+++ challenges/stack-and-queue/lib/src/main/java/StackAndQueue/Node.java
@@ -0,0 +1,18 @@
+package StackAndQueue;
+
+public class Node<T>{
+    public T value;
+    public Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> node) {
+        next = node;
+    }
+}
