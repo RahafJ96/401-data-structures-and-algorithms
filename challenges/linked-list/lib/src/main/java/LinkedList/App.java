2095b56ed9f690653456de1925ad67cb092a2426
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-09 11:56:29 +0300

    copy from linked list new

--- challenges/linked-list/lib/src/main/java/LinkedList/App.java
+++ challenges/linked-list/lib/src/main/java/LinkedList/App.java
@@ -1,30 +1,22 @@
 package LinkedList;
 
 public class App {
+    public boolean someLibraryMethod() {
+        return true;
+    }
     public static void main(String[] args) {
 
-        LinkedList list1 = new LinkedList();
-        list1.append("A");
-        list1.append("B");
-        list1.append("C");
-        list1.append("D");
-        list1.append("E");
-        list1.append("F");
+        LinkedList linkedList = new LinkedList();
 
-        list1.insertAfter("GG","C");
-        list1.insertBefore("W","B");
+        linkedList.insert("Hi");
+        linkedList.insert("I");
+        linkedList.insert("am");
+        linkedList.insert("Rahaf");
 
-        Node current = list1.head;
-        while (current != null) {
-            //System.out.println(current.value);
-            current = current.getNext();
-        }
+        System.out.println(linkedList.size());
+        System.out.println(linkedList.includes("Rahaf"));
 
-        System.out.println(list1.dataToString());
-        System.out.println(list1.includes(3));
-        System.out.println(list1.includes(5));
-        System.out.println(list1.includes(10));
+        System.out.println(linkedList);
 
-        System.out.println(list1.toString());
     }
 }
