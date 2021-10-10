0f6df293c2fc70e961d6531d86d48b196ca28c76
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-10 19:27:28 +0300

    challenge 10

--- challenges/stack-and-queue/lib/src/main/java/StackAndQueue/Library.java
+++ challenges/stack-and-queue/lib/src/main/java/StackAndQueue/Library.java
@@ -5,12 +5,16 @@ package StackAndQueue;
 
 public class Library {
     public static void main(String[] args) {
+
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
+
         System.out.println(stack);
+
         stack.pop();
+
         System.out.println(stack);
         System.out.println(stack.isEmpty());
         System.out.println(stack.peek());
--- challenges/stack-and-queue/lib/src/main/java/StackAndQueue/LinkedList.java
+++
@@ -1,5 +0,0 @@
-package StackAndQueue;
-
-public class LinkedList {
-
-}
