c3f678f7bcd00ae0dd84ca12b7cc2ac231218637
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-10 19:05:18 +0300

    challenge 10

---
+++ challenges/stack-and-queue/lib/src/main/java/StackAndQueue/Stack.java
@@ -0,0 +1,58 @@
+package StackAndQueue;
+
+public class Stack<T> {
+
+    Node top;
+
+    public void push(int value){
+        Node newNode = new Node(value);
+        if (top == null){
+            top = newNode;
+        }else{
+            Node temp = top;
+            top = newNode;
+            newNode.next = temp;
+        }
+        System.out.println("The value " + value + " pushed to the Stack");
+    }
+
+    public int pop(){
+        int popped=0;
+
+        if(top == null){
+            throw new IllegalArgumentException("Empty");
+        }else{
+            popped = top.getValue();
+            top = top.next;
+        }
+        return popped;
+    }
+
+    public int peek(){
+        if(top == null){
+            throw new IllegalArgumentException("Empty");
+        }else{
+            return top.getValue();
+        }
+    }
+
+    public boolean isEmpty(){
+        if(top == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String stackValue = "Stack { ";
+        Node trav = top;
+        while (trav != null){
+            stackValue += trav.getValue() + " --> ";
+            trav=trav.next;
+        }
+        stackValue += "NULL }";
+        return stackValue;
+    }
+}
