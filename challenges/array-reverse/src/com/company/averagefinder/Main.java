3d32810280603f80d235e2ad9e112848788fb79c
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-10-15 18:04:04 +0300

    add to the table + readme

--- challenges/array-reverse/src/com/company/averagefinder/Main.java
+++ challenges/array-reverse/src/com/company/averagefinder/Main.java
@@ -1,7 +1,15 @@
 package com.company.averagefinder;
 
 public class Main {
-    static int[] reverseArray (int[] arr) {
+    public static void main(String[] args) {
+        int x[] = {1,2,3,4,5};
+        int y [] = reverseArray(x);
+        for (int i = 0; i < x.length; i++) {
+            System.out.println(y[i]);
+        }
+    }
+
+    public static int[] reverseArray (int[] arr) {
         int newArr[] = new int[arr.length];
         int counter = 0;
         for (int i = arr.length -1 ; i >= 0 ; i--) {
@@ -10,12 +18,5 @@ public class Main {
         }
 
         return newArr;
-    }
-    public static void main(String[] args) {
-        int x[] = {1,2,3,4,5};
-        int y [] = reverseArray(x);
-        for (int i = 0; i < x.length; i++) {
-            System.out.println(y[i]);
-        }
     }
 }
--- out/production/401-data-structures-and-algorithms/README.md
+++
@@ -1,8 +0,0 @@
-# Reverse an Array
-Write a function called reverseArray which takes an array as an argument. Without utilizing any of the built-in methods available to your language, return an array with elements in reversed order.
-## Whiteboard Process
-![READ01](C:\Users\AB\401course\401-data-structures-and-algorithms\challenges\array-reverse\challange01.PNG)
-
-## Approach & Efficiency
-Looping through the array once in addition to pushing elements to a new array with a time complexity of O(n) .
-
